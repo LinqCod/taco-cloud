09c615602b02b153627a0c4ea7d1db9bdc505288
linqcod <linqcod@yandex.ru>
2021-12-01 23:56:05 +0300

    Initial commit

---
+++ src/main/java/com/linqcod/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.linqcod.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
